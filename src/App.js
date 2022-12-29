8e3d88db96073f21dc0cf5707edaa8c3f4c8948b
Aditya-Kanojia <aditya05kanojia@gmail.com>
2022-12-29 14:27:09 +0530

    changes to component folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Chat</h1>
       <p>Hiiashdoihaso</p>
+      <Home />
     </div>
   );
 }
